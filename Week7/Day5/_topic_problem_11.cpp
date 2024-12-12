26c75714722ab8830789ba35a1e0c0ea18343821
Mahfuzur Rahman <mahfuzshakib301@gmail.com>
2024-12-12 23:52:26 +0600

    Add files via upload
    Uploaded week7 day5

---
+++ Week7/Day5/_topic_problem_11.cpp
@@ -0,0 +1,33 @@
+
+/*    ~ Bismillahir Rahmanir Rahim ~    */
+/*         ~ Rabbi Zidni Ilma ~         */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define Mahfuz_Shakib ios_base:: sync_with_stdio(0); cin.tie(0);
+
+#define int long long
+void Dekhen_Ja_Bhalo_Mone_Hoy()
+{
+    int n;  cin >> n;
+    int ans=0;
+    while(n--)
+    {
+        int x; cin >> x;
+        ans=(ans|x);
+    }
+    cout << ans;
+}
+signed main()
+{
+    Mahfuz_Shakib
+
+    //int tc=1;
+    int tc;  cin>>tc;
+    while(tc--)
+    {
+        Dekhen_Ja_Bhalo_Mone_Hoy();
+        cout<<endl;
+    }
+    return 0;
+}
