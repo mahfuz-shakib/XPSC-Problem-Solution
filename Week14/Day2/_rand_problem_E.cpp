fc6e1f5d38a40414e0a21b686db32893cc26506e
Mahfuzur Rahman <mahfuzshakib301@gmail.com>
2025-01-31 23:49:14 +0600

    Add files via upload
    uploaded week14 day2

---
+++ Week14/Day2/_rand_problem_E.cpp
@@ -0,0 +1,49 @@
+/*    ~ Bismillahir Rahmanir Rahim ~    */
+/*         ~ Rabbi Zidni Ilma ~         */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define Mahfuz_Shakib ios_base:: sync_with_stdio(0); cin.tie(0);
+#define b begin()
+#define e end()
+#define rb rbegin()
+#define re rend()
+#define yes cout<<"YES"
+#define no cout<<"NO"
+#define minus1; { cout<<-1; return; }
+#define zero; { cout<<0; return; }
+#define nl cout<<endl
+#define int long long
+const int Mod=1e9+7;
+void solution()
+{
+    int n; cin >> n;
+    vector<int>v(n);
+    for(auto &x:v) cin >> x;
+    map<int,int>mp;
+    int ans=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                mp[ v[i]+v[j] ]++;
+                ans = max(ans, mp[ v[i]+v[j] ]);
+            }
+        }
+    }
+    cout << ans/2;
+}
+signed main()
+{
+    Mahfuz_Shakib
+
+    int tc=1;
+    //int tc; cin>>tc;
+    while(tc--)
+    {
+        solution(); nl;
+    }
+    return 0;
+}
