9dd66629fefd7d835188c5801c1d8d263c5006be
Mahfuzur Rahman <mahfuzshakib301@gmail.com>
2025-02-06 00:08:18 +0600

    Add files via upload

---
+++ Week14/Day6/_topic_problem_F.cpp
@@ -0,0 +1,49 @@
+/*    ~ Bismillahir Rahmanir Rahim ~    */
+/*         ~ Rabbi Zidni Ilma ~         */
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template<typename T> using pbds = tree<T, null_type, less_equal<T>,rb_tree_tag, tree_order_statistics_node_update>;
+#define Mahfuz_Shakib ios_base:: sync_with_stdio(0); cin.tie(0);
+#define b begin()
+#define e end()
+#define rb rbegin()
+#define re rend()
+#define yes cout<<"YES"
+#define no cout<<"NO"
+#define minus1; { cout<<-1; return; }
+#define zero; { cout<<0; return; }
+#define nl cout<<endl
+#define int long long
+const int Mod=1e9+7;
+void solution()
+{
+    int n; cin >> n ;
+    pbds<int>s;
+    int ans=0;
+    vector<int>v(n);
+    for(auto &x:v) cin >> x;
+    for(int i=v.size()-1;i>=0;i--)
+    {
+        s.insert(v[i]);
+        ans+=s.order_of_key(v[i]);
+    }
+    cout << ans;
+}
+signed main()
+{
+    Mahfuz_Shakib
+
+    //int tc=1;
+    int tc; cin>>tc;
+    while(tc--)
+    {
+        solution(); nl;
+    }
+    return 0;
+}
+
+
