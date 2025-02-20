3bd14b024bc28e2e45bd4a5cb0d53b80e55e6ba8
Mahfuzur Rahman <mahfuzshakib301@gmail.com>
2025-02-21 00:14:30 +0600

    Add files via upload

--- Week16/Day2/_topic_problem_A.cpp
+++ Week16/Day2/_topic_problem_A.cpp
@@ -19,7 +19,7 @@ template<typename T> using pbds = tree<T, null_type, less_equal<T>,rb_tree_tag, 
 #define nl cout<<endl
 #define int long long
 const int Mod=1e9+7;
-const int N=3e5;
+const int N=3e5+9;
 vector<int>v[N], op(N);
 string s;
 void dfs(int x)
@@ -27,14 +27,14 @@ void dfs(int x)
     if(v[x][0])
     {
         int y = v[x][0];
-        if(s[x-1]=='L') op[y] = op[x] + 1;
+        if(s[x-1]!='L') op[y] = op[x] + 1;
         else op[y]=op[x];
         dfs(y);
     }
     if(v[x][1])
     {
         int y = v[x][1];
-        if(s[x-1]=='R') op[y] = op[x] + 1;
+        if(s[x-1]!='R') op[y] = op[x] + 1;
         else op[y]=op[x];
         dfs(y);
     }
@@ -44,28 +44,25 @@ signed main()
     Mahfuz_Shakib
 
     //int tc=1;
-    int tc;
-    cin>>tc;
+    int tc;     cin>>tc;
     while(tc--)
     {
-        int n;
-        cin >> n;
+        int n;     cin >> n;
         cin >> s;
-        for(int i=0; i<n; i++)
+        for(int i=0; i<=n; i++)
         {
             v[i].clear();
             op[i]=0;
         }
         for(int i=1; i<=n; i++)
         {
-            int x,y;
-            cin >> x >> y;
+            int x,y;            cin >> x >> y;
             v[i].push_back(x);
             v[i].push_back(y);
         }
         dfs(1);
         int ans=INT_MAX;
-        for(int i=0; i<n; i++)
+        for(int i=1; i<=n; i++)
         {
             if(v[i][0]==0 && v[i][1]==0)
             {
