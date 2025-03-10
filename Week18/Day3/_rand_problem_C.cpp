4ccb383d22291f6cf4c70f6d3a79fe3581c56877
Mahfuzur Rahman <mahfuzshakib301@gmail.com>
2025-03-10 23:30:02 +0600

    Add files via upload

---
+++ Week18/Day3/_rand_problem_C.cpp
@@ -0,0 +1,52 @@
+/*    ~ Bismillahir Rahmanir Rahim ~    */
+/*         ~ Rabbi Zidni Ilma ~         */
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template<typename T> using pbds = tree<T, null_type, less_equal<T>,rb_tree_tag, tree_order_statistics_node_update>;
+#define Mahfuz_Shakib ios_base:: sync_with_stdio(0); cin.tie(0);
+#define b begin()
+#define e end()
+#define rb rbegin()
+#define re rend()
+#define yes cout<<"YES"
+#define no cout<<"NO"
+#define minus1; { cout<<-1; return; }
+#define zero; { cout<<0; return; }
+#define nl cout<<endl
+#define int long long
+const int Mod=1e9+7;
+void solution()
+{
+    int1(n);
+    string1(s);
+    int ans=0;
+    bool f=false;
+    for(int i=0;i<n-1;i++)
+    {
+        if(s[i]==s[i+1] && s[i]=='*')
+        {
+            f=true;
+            break;
+        }
+        if(s[i]=='@') ans++;
+    }
+    if(!f && s.back()=='@') ans++;
+    cout << ans;
+}
+signed main()
+{
+    Mahfuz_Shakib
+
+    //int tc=1;
+    int tc; cin>>tc;
+    while(tc--)
+    {
+        solution(); nl;
+    }
+    return 0;
+}
+
---
+++ Week18/Day3/_rand_problem_F.cpp
@@ -0,0 +1,33 @@
+/*    ~ Bismillahir Rahmanir Rahim ~    */
+/*         ~ Rabbi Zidni Ilma ~         */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define Mahfuz_Shakib ios_base:: sync_with_stdio(0); cin.tie(0);
+
+#define int long long
+void Dekhen_Ja_Bhalo_Mone_Hoy()
+{
+    int n; cin >> n;
+    int s=0;
+    for(int i=1;i<=n;i++)
+    {
+        int x; cin >> x;
+        if(i==n-1) s-=x;
+        else s+=x;
+    }
+    cout << s;
+}
+signed main()
+{
+    Mahfuz_Shakib
+
+    //int tc=1;
+    int tc;  cin>>tc;
+    while(tc--)
+    {
+        Dekhen_Ja_Bhalo_Mone_Hoy();   cout<<endl;
+    }
+    return 0;
+}
+ 
